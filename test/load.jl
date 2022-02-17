da90ed787dbfa09aced64e909f9bfc394faf1e3d
1ozturkbe <1ozturkbe@gmail.com>
2022-02-17 13:05:23 +0000

    Preliminary version of hyperplane_cart.

--- test/load.jl
+++ test/load.jl
@@ -10,9 +10,9 @@ using Test
 using Random
 
 global MOI = MathOptInterface
-global CPLEX_SILENT = with_optimizer(CPLEX.Optimizer, CPX_PARAM_SCRIND = 0)
 Random.seed!(1);
 MOI.Silent() = true;
 include("../src/OptimalTrees.jl")
 using .OptimalTrees
 global OT = OptimalTrees
+CPLEX_SILENT = OptimalTrees.CPLEX_SILENT
--- test/src.jl
+++ test/src.jl
@@ -1,13 +1,5 @@
-""" Tests MIO-based tree learning. """
-# function test_training()
-    path = "data/iris.data"
-    csv_data = CSV.File(path, header=false)
-    iris_names = ["sepal_len", "sepal_wid", "petal_len", "petal_wid", "class"]
-    df = DataFrame(csv_data)
-    rename!(df, iris_names)
-    dropmissing!(df)
-
-    # Checking BinaryNode
+""" Tests the functionalities of BinaryNode. """
+function test_binarynode()
     bn = BinaryNode(1)
     leftchild(bn, BinaryNode(2))
     rightchild(bn, BinaryNode(3))
@@ -15,10 +7,26 @@
     @test all([child.idx for child in children(bn)] .== [2,3]) &&
         isempty(children(bn.right.right)) && length(alloffspring(bn)) == 3
     
-    deleted_child = bn.right.right
     delete_children!(bn.right)
     @test isnothing(bn.parent) && isnothing(bn.right.right) && 
         isempty(children(bn.right)) && length(alloffspring(bn)) == 2
+
+    @test_throws ErrorException set_split_values!(bn.right, [1,2,3], 4)
+    @test_throws ErrorException set_classification_label!(bn, 5)
+end
+
+
+
+""" Tests MIO-based tree learning. """
+function test_training()
+    path = "data/iris.data"
+    csv_data = CSV.File(path, header=false)
+    iris_names = ["sepal_len", "sepal_wid", "petal_len", "petal_wid", "class"]
+    df = DataFrame(csv_data)
+    rename!(df, iris_names)
+    dropmissing!(df)
+
+
 
     # Checking MIOTree
     d = MIOTree_defaults()
@@ -57,7 +65,7 @@
     # @test all(apply(mt, X) .== Y)
     # For some reason, optimal trees don't seem to be optimal...
     # FIgure out why I'm getting non-zero 
-# end
+end
 
 # test_training()
 
@@ -73,5 +81,115 @@ function add_depth(mt::MIOTree)
         push!(new_leaves, leaf.right)
     end
     mt.leaves = new_leaves
+    return
+end
+
+mt = build_MIOTree(CPLEX_SILENT; max_depth = 6, minbucket = 0.03)
+# Sanitize Y for SVM
+path = "data/iris.data"
+csv_data = CSV.File(path, header=false)
+iris_names = ["sepal_len", "sepal_wid", "petal_len", "petal_wid", "class"]
+df = DataFrame(csv_data)
+rename!(df, iris_names)
+dropmissing!(df)
+
+X = Matrix(df[:,1:4])
+Y = Array(df[:,5])
+Y_bin = []
+for val in Y
+    if val == "Iris-setosa"
+        push!(Y_bin, "Iris-versicolor")
+    else
+        push!(Y_bin, val)
+    end
+end
+Y = Y_bin
+
+function hyperplane_cart(mt::MIOTree, X::Matrix, Y::Array)
+    n_samples, n_vars = size(X)
+    classes = sort(unique(Y))
+    minpoints = ceil(n_samples * get_param(mt, :minbucket))
+    delete_children!(mt.root)
+    mt.leaves = [mt.root]
+    mt.nodes = [mt.root]
+    counts = Dict((i => count(==(i), Y)) for i in unique(Y))
+    maxval = 0
+    maxkey = ""
+    for (key, val) in counts
+        global maxval
+        global maxkey
+        if val ≥ maxval
+            maxval = val
+            maxkey = key
+        end
+    end
+    valid_leaves = [mt.root] # Stores leaves ready for SVM cuts. 
+    ct = 1
+    point_idxs = Dict(mt.root.idx => collect(1:n_samples))
+    while !isempty(valid_leaves)
+        global ct
+        global point_idxs
+        global valid_leaves
+        leaf = popfirst!(valid_leaves)
+        @info "Trying $(leaf.idx)..."
+        leaf.a, leaf.b = SVM(X[point_idxs[leaf.idx], :],
+                            Y[point_idxs[leaf.idx]], CPLEX_SILENT)
+
+        # Checking left child, and adding to valid_leaves if necessary
+        ct += 1
+        leftchild(leaf, BinaryNode(ct))
+        left_idxs = findall(x -> x <= 0, 
+            [sum(leaf.a .*X[i, :]) - leaf.b for i = point_idxs[leaf.idx]])
+        point_idxs[leaf.left.idx] = point_idxs[leaf.idx][left_idxs]
+        n_pos_left = sum(Y[left_idxs].== classes[2])
+        n_neg_left = sum(Y[left_idxs] .== classes[1])
+
+        # Checking right child, and adding to valid leaves if necessary
+        ct += 1
+        rightchild(leaf, BinaryNode(ct))
+        right_idxs = findall(x -> x > 0, 
+        [sum(leaf.a .*X[i, :]) - leaf.b for i = point_idxs[leaf.idx]])
+        point_idxs[leaf.right.idx] = point_idxs[leaf.idx][right_idxs]
+        n_pos_right = sum(Y[right_idxs].== classes[2])
+        n_neg_right = sum(Y[right_idxs] .== classes[1])
+
+        # Setting labels
+        if n_pos_left/n_neg_left > 1 
+            set_classification_label!(leaf.left, classes[2])
+        elseif n_pos_left/n_neg_left == 1
+            @warn "Leaf $(leaf.left.idx) has samples that are split 50/50. Will set based on label of sibling."
+        else
+            set_classification_label!(leaf.left, classes[1])
+        end
+        if n_pos_right/n_neg_right > 1 
+            set_classification_label!(leaf.right, classes[2])
+        elseif n_pos_right/n_neg_right == 1
+            @warn "Leaf $(leaf.right.idx) has samples that are split 50/50. Will set based on label of sibling."
+        else
+            set_classification_label!(leaf.right, classes[1])
+        end
+        
+        # Pruning if necessary, 
+        # and choosing whether leaves should be added to queue. 
+        if leaf.left.label == leaf.right.label
+            point_idxs[leaf.idx] = union(point_idxs[leaf.left.idx], point_idxs[leaf.right.idx])
+            delete!(point_idxs, leaf.left.idx)
+            delete!(point_idxs, leaf.right.idx)
+            parent_label = leaf.left.label
+            delete_children!(leaf)
+            leaf.label = parent_label
+        else
+            left_error_rate = abs(n_pos_left - n_neg_left)/(2*(length(left_idxs)))                
+            right_error_rate = abs(n_pos_right - n_neg_right)/(2*(length(right_idxs)))
+            if left_error_rate != 0 && abs(n_pos_left - n_neg_left) ≥ minpoints
+                push!(valid_leaves, leaf.left)
+            end
+            if right_error_rate != 0 && abs(n_pos_right - n_neg_right) ≥ minpoints
+                push!(valid_leaves, leaf.right)
+            end
+            # Since leaf is no longer a leaf, deleting its point indices. 
+            delete!(point_idxs, leaf.idx)
+        end
+    end
     return
 end
